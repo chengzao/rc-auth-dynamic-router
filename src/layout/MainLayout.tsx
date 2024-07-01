243ef9cc492f726bc56cb5aed1e185c11f78dd51
chengzao <czhlink@163.com>
2024-07-01 16:32:04 +0800

    feat: update

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,21 +1,36 @@
 import { Suspense } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import { useAppContext } from '@/app-context';
 import { Loading } from '@/components/Loading';
+
+import styles from '@/layout.module.less'
 
 function Layout() {
   const { menus } = useAppContext();
 
+  const logout = () => {
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
+
   return (
-    <div style={{ display: 'flex', gap: 20 }}>
-      <ul>
-        {menus.map((menu, index) => (
-          <li key={index}><Link to={menu.path}>{menu.name}</Link></li>
-        ))}
-      </ul>
-      <Suspense fallback={<Loading />}>
-        <Outlet />
-      </Suspense>
+    <div className={styles.layout}>
+      <nav className={styles.nav}>
+        <h1 className={styles.header}>KMS DashBoard</h1>
+        <ul className={styles.container}>
+          {menus.map((menu, index) => (
+            <li key={index}><NavLink to={menu.path} className={({ isActive }) => isActive ? styles.active : ''}>{menu.name}</NavLink></li>
+          ))}
+        </ul>
+        <div className={styles.footer}>
+          <button onClick={logout}>logout</button>
+        </div>
+      </nav>
+      <main className={styles.main}>
+        <Suspense fallback={<Loading />}>
+          <Outlet />
+        </Suspense>
+      </main>
     </div>
   )
 }
