edcd50a7007004a04ccb4e8164a144e5d191cd93
chengzao <czhlink@163.com>
2024-07-05 18:23:22 +0800

    feat: update mock

--- mock/list.mock.ts
+++ mock/list.mock.ts
@@ -1,12 +1,35 @@
 import { defineMock } from "vite-plugin-mock-dev-server";
 
+const roles = ["admin", "user"];
+
 export default defineMock({
   url: "/api/list",
   method: ["GET"],
-  body() {
-    return {
-      code: 200,
-      data: "this is a list data",
-    };
+
+  response(req, res) {
+    const token = req.headers["authorization"];
+
+    if (!token || !roles.includes(token)) {
+      res.statusCode = 401;
+      res.setHeader("Content-Type", "application/json");
+
+      res.end(
+        JSON.stringify({
+          code: 401,
+          message: "unauthorized",
+        })
+      );
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        code: 200,
+        data: "this is a list data",
+      })
+    );
   },
 });
--- mock/user.mock.ts
+++ mock/user.mock.ts
@@ -1,47 +1,54 @@
 import { defineMock } from "vite-plugin-mock-dev-server";
 
+const roles = ["admin", "user"];
+
+const baseRoles = [
+  {
+    name: "page1",
+    roleItemKey: "dashboard:page1",
+  },
+];
+
+const adminRoles = [
+  ...baseRoles,
+  {
+    name: "page2",
+    roleItemKey: "dashboard:page2",
+  },
+  {
+    name: "page3",
+    roleItemKey: "dashboard:page3",
+  },
+];
+
 export default defineMock({
   url: "/api/user",
   method: ["GET"],
-  body(request) {
-    const token = request.headers["authorization"];
-    console.log("token", token);
 
-    if (!token) {
-      return {
-        code: 401,
-        data: [],
-      };
+  response(req, res) {
+    const token = req.headers["authorization"];
+
+    if (!token || !roles.includes(token)) {
+      res.statusCode = 401;
+      res.setHeader("Content-Type", "application/json");
+
+      res.end(
+        JSON.stringify({
+          code: 401,
+          message: "unauthorized",
+        })
+      );
+
+      return;
     }
 
-    if (token === "admin") {
-      return {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
         code: 200,
-        data: [
-          {
-            name: "page1",
-            roleItemKey: "dashboard:page1",
-          },
-          {
-            name: "page2",
-            roleItemKey: "dashboard:page2",
-          },
-          {
-            name: "page3",
-            roleItemKey: "dashboard:page3",
-          },
-        ],
-      };
-    }
-
-    return {
-      code: 200,
-      data: [
-        {
-          name: "page1",
-          roleItemKey: "dashboard:page1",
-        },
-      ],
-    };
+        data: token === "admin" ? adminRoles : baseRoles,
+      })
+    );
   },
 });
