91e6ff1e552afd765b42aedc9591a4e0078680b0
chengzao <czhlink@163.com>
2024-07-05 18:13:03 +0800

    feat: update mock

--- mock/login.mock.ts
+++ mock/login.mock.ts
@@ -1,21 +1,49 @@
 import { defineMock } from "vite-plugin-mock-dev-server";
 
+const roles = ["admin", "user"];
+
 export default defineMock({
   url: "/api/login",
   method: ["POST"],
-  body(request) {
-    const { username } = request.body;
+  response(req, res) {
+    const { body } = req;
 
-    if (username === "admin") {
-      return {
-        code: 200,
-        token: "admin",
-      };
+    if (!body) {
+      res.statusCode = 400;
+      res.setHeader("Content-Type", "application/json");
+
+      res.end(
+        JSON.stringify({
+          code: 400,
+          message: "参数不对",
+        })
+      );
+      return;
     }
 
-    return {
-      code: 200,
-      token: "user",
-    };
+    const { username } = body;
+
+    if (!roles.includes(username)) {
+      res.statusCode = 401;
+      res.setHeader("Content-Type", "application/json");
+
+      res.end(
+        JSON.stringify({
+          code: 401,
+          message: "没有权限",
+        })
+      );
+      return;
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+
+      res.end(
+        JSON.stringify({
+          code: 200,
+          token: username === "admin" ? "admin" : "user",
+        })
+      );
+    }
   },
 });
